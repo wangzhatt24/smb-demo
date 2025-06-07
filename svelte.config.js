236e518fa66e7009e1e6e13171a76edae09da360
Tyler <quangnhatpr9@gmail.com>
2025-06-07 08:50:41 +0700

    new svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		fallback: 'index.html'
-	}) }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: "index.html",
+      pages: "build",
+      assets: "build",
+      precompress: false,
+      strict: true,
+    }),
+  },
 };
 
 export default config;
